1beb644533f9349998a5e55b2d21c8e09a32790c
lazirst <rushgankteam@gmail.com>
2026-07-03 22:25:27 +0700

    Disable API key validation in handler
    Comment out API key validation logic in handler.

--- api/index.py
+++ api/index.py
@@ -5,15 +5,15 @@ import os # Jangan lupa import os
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         # Mengambil kunci dari Environment Variable Vercel
-        RAHASIA_DARI_VERCEL = os.environ.get('KEY') 
+        #RAHASIA_DARI_VERCEL = os.environ.get('KEY') 
         
         # Mengambil kunci yang dikirim oleh bot/user
-        key_pengguna = self.headers.get("X-API-KEY")
+        #key_pengguna = self.headers.get("X-API-KEY")
         
         # Pengecekan
-        if key_pengguna != RAHASIA_DARI_VERCEL:
-            self.send_error(403, "Akses ditolak!")
-            return
+        #if key_pengguna != RAHASIA_DARI_VERCEL:
+            #self.send_error(403, "Akses ditolak!")
+            #return
 
         # Jika sukses, kirim data
         self.send_response(200)
